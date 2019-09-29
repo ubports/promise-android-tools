003219fd3da460aac4affb1f348acd79b55e4a9c
Jan Sprinz <neo@neothethird.de>
2019-09-29 15:10:23 +0200

    Structure adb tests

--- tests/unit-tests/test_adb.js
+++ tests/unit-tests/test_adb.js
@@ -54,180 +54,186 @@ describe('Adb module', function() {
       expect(adb.port).to.equal(1234);
     });
   });
-  describe("exec()", function() {
-    it("should call the specified function", function() {
-      const execSpy = sinon.spy();
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execSpy, log: logSpy});
-      adb.exec("This is an argument");
-      expect(execSpy).to.have.been.calledWith("This is an argument");
-    });
-  })
-  describe("execPort()", function() {
-    it("should call an executable with port argument", function() {
-      const execStub = (args, callback) => {
-        exec("node tests/test-data/fake_executable.js " + args.join(" "), callback);
-      };
-      const logStub = sinon.stub();
-      const adb = new Adb({exec: execStub, log: logStub, port: 1234});
-      return adb.execPort().then((r, r2, r3) => {
-        expect(r).to.equal("-P 1234");
+  describe("private functions", function() {
+    describe("exec()", function() {
+      it("should call the specified function", function() {
+        const execSpy = sinon.spy();
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execSpy, log: logSpy});
+        adb.exec("This is an argument");
+        expect(execSpy).to.have.been.calledWith("This is an argument");
+      });
+    })
+    describe("execPort()", function() {
+      it("should call an executable with port argument", function() {
+        const execStub = (args, callback) => {
+          exec("node tests/test-data/fake_executable.js " + args.join(" "), callback);
+        };
+        const logStub = sinon.stub();
+        const adb = new Adb({exec: execStub, log: logStub, port: 1234});
+        return adb.execPort().then((r, r2, r3) => {
+          expect(r).to.equal("-P 1234");
+        });
       });
     });
   });
-  describe("startServer()", function() {
-    it("should kill all servers and start a new one", function() {
-      const execFake = sinon.fake((args, callback) => { callback(); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.startServer().then((r) => {
-        expect(r).to.equal(undefined);
-        expect(execFake).to.have.been.calledTwice;
-        expect(execFake).to.have.been.calledWith(["-P", 5037, "kill-server"]);
-        expect(execFake).to.have.been.calledWith(["-P", 5037, "start-server"]);
-        expect(logSpy).to.have.been.calledTwice;
-        expect(logSpy).to.have.been.calledWith("killing all running adb servers");
-        expect(logSpy).to.have.been.calledWith("starting adb server on port 5037");
+  describe("basic functions", function() {
+    describe("startServer()", function() {
+      it("should kill all servers and start a new one", function() {
+        const execFake = sinon.fake((args, callback) => { callback(); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.startServer().then((r) => {
+          expect(r).to.equal(undefined);
+          expect(execFake).to.have.been.calledTwice;
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "kill-server"]);
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "start-server"]);
+          expect(logSpy).to.have.been.calledTwice;
+          expect(logSpy).to.have.been.calledWith("killing all running adb servers");
+          expect(logSpy).to.have.been.calledWith("starting adb server on port 5037");
+        });
       });
     });
-  });
-  describe("killServer()", function() {
-    it("should kill all servers", function() {
-      const execFake = sinon.fake((args, callback) => { callback(); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.killServer().then((r) => {
-        expect(r).to.equal(undefined);
-        expect(execFake).to.have.been.calledOnce;
-        expect(execFake).to.not.have.been.calledTwice;
-        expect(execFake).to.have.been.calledWith(["-P", 5037, "kill-server"]);
-        expect(logSpy).to.not.have.been.calledTwice;
-        expect(logSpy).to.have.been.calledWith("killing all running adb servers");
+    describe("killServer()", function() {
+      it("should kill all servers", function() {
+        const execFake = sinon.fake((args, callback) => { callback(); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.killServer().then((r) => {
+          expect(r).to.equal(undefined);
+          expect(execFake).to.have.been.calledOnce;
+          expect(execFake).to.not.have.been.calledTwice;
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "kill-server"]);
+          expect(logSpy).to.not.have.been.calledTwice;
+          expect(logSpy).to.have.been.calledWith("killing all running adb servers");
+        });
       });
     });
-  });
-  describe("getSerialno()", function() {
-    it("should return serialnumber", function() {
-      const execFake = sinon.fake((args, callback) => { callback(false, "1234567890ABCDEF\n"); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.getSerialno().then((r) => {
-        expect(r).to.equal("1234567890ABCDEF");
-        expect(execFake).to.have.been.calledOnce;
-        expect(execFake).to.have.been.calledWith(["-P", 5037, "get-serialno"]);
+    describe("getSerialno()", function() {
+      it("should return serialnumber", function() {
+        const execFake = sinon.fake((args, callback) => { callback(false, "1234567890ABCDEF\n"); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.getSerialno().then((r) => {
+          expect(r).to.equal("1234567890ABCDEF");
+          expect(execFake).to.have.been.calledOnce;
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "get-serialno"]);
+        });
+      });
+      it("should return error on invalid return", function() {
+        const execFake = sinon.fake((args, callback) => { callback(false, "This is an invalid string"); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return expect(adb.getSerialno()).to.be.rejectedWith("invalid device id");
       });
     });
-    it("should return error on invalid return", function() {
-      const execFake = sinon.fake((args, callback) => { callback(false, "This is an invalid string"); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return expect(adb.getSerialno()).to.be.rejectedWith("invalid device id");
-    });
-  });
-  describe("shell()", function() {
-    it("should run command on device", function() {
-      const execFake = sinon.fake((args, callback) => { callback(null, "This string is returned over stdout"); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.shell(["one", "two", "three"]).then((r) => {
-        expect(r).to.equal("This string is returned over stdout");
-        expect(execFake).to.have.been.called;
+    describe("shell()", function() {
+      it("should run command on device", function() {
+        const execFake = sinon.fake((args, callback) => { callback(null, "This string is returned over stdout"); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.shell(["one", "two", "three"]).then((r) => {
+          expect(r).to.equal("This string is returned over stdout");
+          expect(execFake).to.have.been.called;
+        });
       });
     });
-  });
-  describe("push()", function() {
-    it("executable should be able to access files");
-    it("should push file");
-    it("should reject if file is inaccessible");
-  });
-  describe("pushArray()", function() {
-    it("should reject on empty array");
-    it("should push files");
-    it("should quietly stop waiting");
-  });
-  describe("getDeviceName()", function() {
-    it("should get device name from getprop", function() {
-      const execFake = sinon.fake((args, callback) => { callback(null, "thisisadevicecodename"); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.getDeviceName().then((r) => {
-        expect(r).to.equal("thisisadevicecodename");
-        expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "getprop", "ro.product.device"]);
-      });
-    });
-    it("should get device name from default prop file");
-    it("should throw an error if prop can't be found");
-  });
-  describe("getOs()", function() {
-    it("should resolve \"ubuntutouch\"", function() {
-      const execFake = sinon.fake((args, callback) => { callback(null, "Contents of the system-image file go here"); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.getOs().then((r) => {
-        expect(r).to.equal("ubuntutouch");
-        expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "cat", "/etc/system-image/channel.ini"]);
-      });
-    });
-    it("should resolve \"android\"", function() {
-      const execFake = sinon.fake((args, callback) => { callback(null, null); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.getOs().then((r) => {
-        expect(r).to.equal("android");
-        expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "cat", "/etc/system-image/channel.ini"]);
-      });
+    describe("push()", function() {
+      it("executable should be able to access files");
+      it("should push file");
+      it("should reject if file is inaccessible");
     });
   });
-  describe("hasAccess()", function() {
-    it("should resolve true", function() {
-      const execFake = sinon.fake((args, callback) => { callback(null, "."); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.hasAccess().then((r) => {
-        expect(r).to.equal(true);
+  describe("convenience functions", function() {
+    describe("pushArray()", function() {
+      it("should reject on empty array");
+      it("should push files");
+      it("should quietly stop waiting");
+    });
+    describe("getDeviceName()", function() {
+      it("should get device name from getprop", function() {
+        const execFake = sinon.fake((args, callback) => { callback(null, "thisisadevicecodename"); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.getDeviceName().then((r) => {
+          expect(r).to.equal("thisisadevicecodename");
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "getprop", "ro.product.device"]);
+        });
+      });
+      it("should get device name from default prop file");
+      it("should throw an error if prop can't be found");
+    });
+    describe("getOs()", function() {
+      it("should resolve \"ubuntutouch\"", function() {
+        const execFake = sinon.fake((args, callback) => { callback(null, "Contents of the system-image file go here"); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.getOs().then((r) => {
+          expect(r).to.equal("ubuntutouch");
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "cat", "/etc/system-image/channel.ini"]);
+        });
+      });
+      it("should resolve \"android\"", function() {
+        const execFake = sinon.fake((args, callback) => { callback(null, null); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.getOs().then((r) => {
+          expect(r).to.equal("android");
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "cat", "/etc/system-image/channel.ini"]);
+        });
+      });
+    });
+    describe("hasAccess()", function() {
+      it("should resolve true", function() {
+        const execFake = sinon.fake((args, callback) => { callback(null, "."); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.hasAccess().then((r) => {
+          expect(r).to.equal(true);
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "echo", "."]);
+        });
+      });
+      it("should resolve false", function() {
+        const execFake = sinon.fake((args, callback) => { callback(true, null, "error: no devices/emulators found"); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.hasAccess().then((r) => {
+          expect(r).to.equal(false);
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "echo", "."]);
+        });
+      });
+      it("should reject", function() {
+        const execFake = sinon.fake((args, callback) => { callback(null, "This is an unexpected reply"); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return expect(adb.hasAccess()).to.have.been.rejectedWith("unexpected response: This is an unexpected reply");
         expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "echo", "."]);
       });
     });
-    it("should resolve false", function() {
-      const execFake = sinon.fake((args, callback) => { callback(true, null, "error: no devices/emulators found"); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.hasAccess().then((r) => {
-        expect(r).to.equal(false);
-        expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "echo", "."]);
+    describe("waitForDevice()", function() {
+      it("should resolve immediately", function() {
+        const execFake = sinon.fake((args, callback) => { callback(null, "."); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return adb.waitForDevice(5).then((r) => {
+          expect(execFake).to.have.been.called;
+          expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "echo", "."]);
+        });
       });
     });
-    it("should reject", function() {
-      const execFake = sinon.fake((args, callback) => { callback(null, "This is an unexpected reply"); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return expect(adb.hasAccess()).to.have.been.rejectedWith("unexpected response: This is an unexpected reply");
-      expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "echo", "."]);
-    });
-  });
-  describe("waitForDevice()", function() {
-    it("should resolve immediately", function() {
-      const execFake = sinon.fake((args, callback) => { callback(null, "."); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return adb.waitForDevice(5).then((r) => {
-        expect(execFake).to.have.been.called;
-        expect(execFake).to.have.been.calledWith(["-P", 5037, "shell", "echo", "."]);
-      });
-    });
-  });
-  describe("stopWaiting()", function() {
-    it("should quietly stop waiting", function() {
-      const execFake = sinon.fake((args, callback) => { callback(true, null, "error: no devices/emulators found"); });
-      const logSpy = sinon.spy();
-      const adb = new Adb({exec: execFake, log: logSpy});
-      return new Promise(function(resolve, reject) {
-        const wait = adb.waitForDevice(5);
-        setTimeout(() => {
-          adb.stopWaiting();
-          expect(wait).to.be.rejectedWith("stopped waiting");
-          resolve();
-        }, 10)
+    describe("stopWaiting()", function() {
+      it("should quietly stop waiting", function() {
+        const execFake = sinon.fake((args, callback) => { callback(true, null, "error: no devices/emulators found"); });
+        const logSpy = sinon.spy();
+        const adb = new Adb({exec: execFake, log: logSpy});
+        return new Promise(function(resolve, reject) {
+          const wait = adb.waitForDevice(5);
+          setTimeout(() => {
+            adb.stopWaiting();
+            expect(wait).to.be.rejectedWith("stopped waiting");
+            resolve();
+          }, 10)
+        });
       });
     });
   });
