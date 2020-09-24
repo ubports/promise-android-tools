e5464e7cd90962763558446664b4c74f02469e25
Jan Sprinz <neo@neothethird.de>
2020-09-24 18:57:11 +0200

    Fix linter errors

--- tests/unit-tests/test_fastboot.js
+++ tests/unit-tests/test_fastboot.js
@@ -119,18 +119,14 @@ describe("Fastboot module", function() {
       });
       it("should reject if bootloader is locked", function() {
         const execFake = sinon.fake((args, callback) => {
-          callback(
-            true,
-            "",
-            "FAILED (remote: 'Bootloader is locked.')"
-          );
+          callback(true, "", "FAILED (remote: 'Bootloader is locked.')");
         });
         const logSpy = sinon.spy();
         const fastboot = new Fastboot({ exec: execFake, log: logSpy });
         return expect(
           fastboot.flash("boot", "/path/to/image")
         ).to.have.been.rejectedWith(
-          'flashing failed: Error: bootloader is locked'
+          "flashing failed: Error: bootloader is locked"
         );
       });
       it("should reject if flashing failed", function() {
