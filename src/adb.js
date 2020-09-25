f9f5d2f2171288c56eca954979ceec65d3ffea47
Jan Sprinz <neo@neothethird.de>
2020-09-26 00:26:41 +0200

    Implement adb sideload

--- src/adb.js
+++ src/adb.js
@@ -250,6 +250,17 @@ class Adb {
     });
   }
 
+  // sideload an ota package
+  sideload(file) {
+    var _this = this;
+    var Exp = /^([0-9]|[a-z])+([0-9a-z]+)$/i;
+    return _this.execCommand([
+      "sideload",
+      common.quotepath(file),
+      common.stdoutFilter("%)")
+    ]);
+  }
+
   //////////////////////////////////////////////////////////////////////////////
   // Convenience functions
   //////////////////////////////////////////////////////////////////////////////
